79388f6925409acdd40cfdb4774e67095dcadb37
kenta-pro <kenta.yuri1026@gmail.com>
2022-07-21 07:16:21 +0900

    develop

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id="contact" className="contact">
+      <div className="container">
+        <div className="contact-contents">
+          <h2>Contact to Me!</h2>
+          <p className="description">
+            質問等ありましたら下記よりご連絡ください。
+          </p>
+          <a href="https://twitter.com/" className="btn">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
